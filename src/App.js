b7319542bbfcdac68a57266581555044713d6a62
Saurabh Dahal <saurabhdahal@Saurabhs-MBP2.fios-router.home>
2021-03-11 17:02:31 -0500

    added state lecture on class components

--- src/App.js
+++ src/App.js
@@ -6,8 +6,13 @@ function App() {
   return (
     
       <div className="App">
-        <Menu itemName={"Calamari"} desc = "Its some fried squid but its good tho" price = {12.99} orders= {12}></Menu>
-        <Menu itemName={"Al Pastor tacos"} desc= {"Tacos made exquisitly with a blend of squid and other secret ingredients "} price = {3.99} orders= {1}></Menu>
+        <Menu itemName={"Calamari"} desc = "Its some fried squid but its good tho" price = {12.99} orders= {12}>
+          <h5>This is the first menu item!!!</h5>
+          <p>This is a child paragraph tag to the MENU component</p>
+        </Menu>
+        <Menu itemName={"Al Pastor tacos"} desc= {"Tacos made exquisitly with a blend of squid and other secret ingredients "} price = {3.99} orders= {1}>
+          <h3>This is the second menu item!</h3>
+        </Menu>
         <Menu itemName= {"Salmon"} desc= {"Omega 3 tho "} price = {30.99} orders= {100}></Menu>
         <Menu itemName= {"Menudo"} desc= {"The menudo be on our menu tho"} price = {10.99} orders= {2}></Menu>
         <Menu itemName= {"Ramen"} desc= {"its not the Top Ramen tho"} price = {15.99} orders= {120}></Menu>
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -13,16 +13,35 @@
 import React, {Component} from 'react';
 
 class Menu extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            orderCount: this.props.orders
+        };
+    }
+    
 
     
     render(){
+
+        const doSomething = ()=>{
+            console.log("you clicked on the order button, for the price of")
+            // alert(`You just clicked to order more ${itemName}`)
+            // alert("wazzzaaaa")
+            this.setState({orderCount: this.state.orderCount+1 })
+            // this.setState({ position: "Off" })
+        }
         const { itemName, desc, price, orders } = this.props;
 
         return <div className="menuItem">
+            
             <h1>{itemName}</h1>
+            {this.props.children}
             <p>Description: {this.props.desc}</p>
             <p>Price: ${this.props.price}</p>
-            <p>Number of orders today: {this.props.orders}</p>
+            <p>Number of orders today: {this.state.orderCount}</p>
+            <button onClick={doSomething}>Order more {itemName}</button>
+            {/* <button onClick={ ()=> alert("This button has been clicked!") }>Click Me</button> */}
            
         </div>
     }
